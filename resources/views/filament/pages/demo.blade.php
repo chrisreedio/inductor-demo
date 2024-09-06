819899533719ac6f35bf952f81afeedd8236f732
Chris Reed <chris@reedtech.us>
2024-09-06 05:39:30 -0500

    ✨ feat: added `PHPVersion` component to demo page
    Enhanced the demo page by including a new `PHPVersion` component.

--- resources/views/filament/pages/demo.blade.php
+++ resources/views/filament/pages/demo.blade.php
@@ -1,7 +1,8 @@
 <x-filament-panels::page>
-    <div wire:ignore class="flex flex-col w-auto p-2 space-y-4">
+    <div wire:ignore class="flex flex-col w-auto p-2 gap-y-4">
         <livewire:vue component="ApplicationCounter" />
         <livewire:vue component="SampleComponent" />
         <livewire:vue component="SampleCounter" />
+        <livewire:vue component="PHPVersion" />
     </div>
 </x-filament-panels::page>
