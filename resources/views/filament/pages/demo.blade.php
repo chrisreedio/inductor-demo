31b0ecd8f6703f0884ccc5889a019af6735ea7d9
Chris Reed <chris@reedtech.us>
2024-09-06 04:49:34 -0500

    ✨ refactor: cleaned up demo.blade.php by removing comments
    Removed unnecessary commented-out code to enhance readability and maintainability.

--- resources/views/filament/pages/demo.blade.php
+++ resources/views/filament/pages/demo.blade.php
@@ -1,12 +1,7 @@
 <x-filament-panels::page>
-    {{--<livewire:vue component="Counter" />--}}
     <div wire:ignore class="flex flex-col w-auto p-2 space-y-4">
-        {{--<livewire:vue component="SampleComponent" />--}}
+        {{--<livewire:vue component="Counter" />--}}
+        <livewire:vue component="SampleComponent" />
         <livewire:vue component="SampleCounter" />
     </div>
-    {{--<livewire:vue>--}}
-    {{--    <div class="border-2 border-red-500 p-4">--}}
-    {{--        Hello! This is a Vue Component wrapped in a Livewire Component.--}}
-    {{--    </div>--}}
-    {{--</livewire:vue>--}}
 </x-filament-panels::page>
