c1882c450d8ed35342817daef26e7a6285a45277
Chris Reed <chris@reedtech.us>
2024-09-06 02:34:52 -0500

    ✨ feat(vite.config): added globSync for dynamic input paths
    Enhanced `vite.config.js` to use `globSync` for dynamic file input paths. Made adjustments to `AdminPanelProvider` to include `viteTheme`.

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,29 @@
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue'
+import { globSync } from 'glob'
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
+            input: globSync('resources/{css,js,Components,views}/**/*.{css,js,vue}'),
+            // input: [
+            //     'resources/css/app.css',
+            //     'resources/js/app.js',
 
-                // Filament Theme
-                'resources/css/filament/admin/theme.css',
+            // Filament Theme
+            // 'resources/css/filament/admin/theme.css',
 
-                // Inductor
-                'resources/views/**/*.blade.php',
-                'resources/js/Components/**/*.js',
-                'resources/js/Components/**/*.vue',
-                'vendor/chrisreedio/inductor/resources/js/**/*.js',
-            ],
+            // Inductor
+            // 'resources/views/**/*.blade.php',
+            // 'resources/js/Components/**/*.js',
+            // 'resources/js/Components/**/*.vue',
+            // 'resources/js/Components/Counter.vue',
+
+            // ...globSync("resources/{css,js,Components}/**/*.{css,js,vue}")
+
+            // 'vendor/chrisreedio/inductor/resources/js/**/*.js',
+            // ],
             refresh: true,
         }),
         vue({
